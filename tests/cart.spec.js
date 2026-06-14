25be25e39486178722354e4fdf41dcdc7fb2e1af
Hernan <onlihernan@gmail.com>
2026-06-14 17:23:37 -0300

    feat: add loggedInPage fixture to centralize login setup

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,22 +1,14 @@
-const { test } = require('@playwright/test');
-const { LoginPage } = require('../pages/loginPage');
+const { test } = require('../fixtures');
 const { InventoryPage } = require('../pages/inventoryPage');
 const { CartPage } = require('../pages/cartPage');
 
-test('validate cart with added product', async ({ page }) => {
-
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  const cartPage = new CartPage(page);
-
-  await loginPage.goto();
-  await loginPage.login('standard_user', 'secret_sauce');
+test('validate cart with added product', async ({ loggedInPage }) => {
+  const inventoryPage = new InventoryPage(loggedInPage);
+  const cartPage = new CartPage(loggedInPage);
 
   await inventoryPage.addProductToCart('Sauce Labs Backpack');
   await inventoryPage.goToCart();
-
   await cartPage.validateProductInCart('Sauce Labs Backpack');
   await cartPage.validatePrice('$29.99');
   await cartPage.validateQuantity('1');
-
-}); 
+});
